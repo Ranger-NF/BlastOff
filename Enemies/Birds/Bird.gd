1d04f3906fb490c1545e9318d604eb0781e9c788
Ranger <yemzero0@gmail.com>
2024-05-26 11:40:37 +0530

    Fixed scripts and inheritance 📜

---
+++ Enemies/Birds/Bird.gd
@@ -0,0 +1,4 @@
+extends "res://Enemies/Obstacle.gd"
+
+func get_group_spawn_points() -> void:
+    pass
---
+++ Enemies/Obstacle.gd
@@ -0,0 +1,60 @@
+class_name Obstacle
+extends Area2D
+
+@export var acceleration = 30
+@export var can_be_grouped: bool = false
+
+var can_move: bool = false
+var move_vec: Vector2 = Vector2.ZERO
+
+enum {
+    LEFT,
+    RIGHT
+}
+
+# For objects that move across the screen
+@export var horizontal_speed: float = 0 # Should be constant speed
+
+var direction_to_move: int # Left or Right
+
+func _ready() -> void:
+    can_move = true
+    horizontal_speed = randf_range(0, 10) # Randomize
+
+    var horizontal_screen_size = get_viewport_rect().size.x
+    if horizontal_speed != 0 and self.position.x > (horizontal_screen_size / 2): # If on the right side move to left
+        direction_to_move = LEFT
+    else:
+        direction_to_move = RIGHT
+
+func free_fall(delta) -> void:
+    move_vec.y = (GameManager.rocket_speed * delta)
+
+    self.position.y += move_vec.y
+
+func _physics_process(delta: float) -> void:
+    if not can_move:
+        return
+
+    free_fall(delta)
+    if not horizontal_speed == 0:
+
+        match direction_to_move:
+            RIGHT:
+                move_vec.x += (horizontal_speed * delta)
+                $AnimatedSprite2D.flip_h = false
+            LEFT:
+                move_vec.x += -1 * (horizontal_speed * delta) # Make it move to left side (-ve x)
+                $AnimatedSprite2D.flip_h = true
+
+        move_vec.x = limit_speed(move_vec.x, horizontal_speed)
+        self.position.x += move_vec.x
+
+func limit_speed(current_speed: float, max_speed: float) -> float:
+    if current_speed > max_speed:
+        return max_speed
+    else:
+        return current_speed
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+    queue_free()
